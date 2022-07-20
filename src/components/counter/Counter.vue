e9ff7f13fa5d898219560ea33e2a46c7e5777d79
Xie Yuheng <xyheme@gmail.com>
2022-07-21 03:59:03 +0800

    up

---
+++ src/components/counter/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+const state = reactive({
+  count: 0,
+})
+
+onMounted(() => {
+  setInterval(() => {
+    state.count++
+  }, 100)
+})
+</script>
+
+<template>
+  <div>Count: {{ state.count }}</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-3xl text-red-500 font-bold">
+  <h1 class="text-3xl text-orange-500 font-bold">
     <div class="p-2">Hello world!</div>
+
+    <Counter></Counter>
   </h1>
 </template>
