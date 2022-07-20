6d27c21838b4628fe44cb4c33d4382733321cb07
Xie Yuheng <xyheme@gmail.com>
2022-07-21 03:17:59 +0800

    up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,9 @@
-<script setup>
-import { Lang } from "../states/Lang"
-
-const lang = new Lang()
-
-const x = 1
-</script>
+<script setup></script>
 
 <template>
   <Observer>
     <h1 class="text-3xl font-bold">
       <div class="p-2">Hello world!</div>
-      <div class="p-2">
-        {{ lang.count }}
-      </div>
     </h1>
   </Observer>
 </template>
