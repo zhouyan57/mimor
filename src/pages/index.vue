3126a84f9a417bb11dbec4d6eaf032a7e89a87d6
Xie Yuheng <xyheme@gmail.com>
2022-07-21 03:40:28 +0800

    up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <Observer>
-    <h1 class="text-3xl text-red-500 font-bold">
-      <div class="p-2">Hello world!</div>
-    </h1>
-  </Observer>
+  <h1 class="text-3xl text-red-500 font-bold">
+    <div class="p-2">Hello world!</div>
+  </h1>
 </template>
