e5ad5d7524f4c82108437b5ce80be3305efaf10b
Xie Yuheng <xyheme@gmail.com>
2022-08-08 19:42:09 +0800

    up

--- src/models/Theme.ts
+++ src/models/Theme.ts
@@ -1,7 +1,7 @@
 import colors from "tailwindcss/colors.js"
 
 export class Theme {
-  constructor(public name: string = "stone") {}
+  constructor(public name: string = "orange") {}
 
   get backgroundColor(): string {
     return (colors as any)[this.name][50]
