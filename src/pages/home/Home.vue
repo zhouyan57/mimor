afc8009fa537b792c03bacda104816e79509be84
Xie Yuheng <xyheme@gmail.com>
2022-08-09 15:07:53 +0800

    extract mimors to `public/examples/`

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,62 +1,50 @@
 <script setup lang="ts">
-import { reactive } from "vue"
+import { reactive, onMounted } from "vue"
 import Mimor from "../../components/mimor/Mimor.vue"
-
-const text = `
-<填空>
-君子知夫不全不粹之不足以为美也
-故诵数以贯之 <空>思索</空>以通之
-为其人以处之 除其害者以持养之
--- 荀子 劝学
-</填空>
-
-<question>
-  What is Bayesian interpretation of probability?
-
-  <answer>
-    Probabilities encode degrees of belief about events in the world.
-
-    And data are used to strengthen, update, or weaken those degrees of belief.
-  </answer>
-</question>
-
-<question>
-  What are some good examples of "association is not causation"?
-
-  <answer>
-    Sleeping with shoes on, and waking up with headache.
-
-    Confounder: drinking the night before.
-  </answer>
-</question>
-`
 
 const state = reactive({
   finished: false,
   flag: true,
+  text: "",
+  loading: true,
 })
 
 function again() {
   state.finished = false
   state.flag = !state.flag
 }
+
+async function loadText() {
+  const response = await fetch("/examples/sisuo.mimor")
+  const text = await response.text()
+  console.log({ text })
+  state.text = text
+  state.loading = false
+}
+
+onMounted(() => {
+  loadText()
+})
 </script>
 
 <template>
   <div class="h-full flex flex-col">
     <div class="text-3xl">Demo 1</div>
 
+    <div v-if="state.loading">Loading...</div>
+
     <Mimor
-      v-if="!state.finished"
+      v-else-if="!state.finished"
       class="h-full overflow-hidden"
       :key="state.flag.toString()"
       :options="{
-        text,
+        text: state.text,
         onFinished: () => {
           state.finished = true
         },
       }"
     />
+
     <div v-else class="h-full flex flex-col items-start justify-between">
       <div></div>
 
