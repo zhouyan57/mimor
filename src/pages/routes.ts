48abfe726aab2247fd4e15be90a544f89597cfc4
Xie Yuheng <xyheme@gmail.com>
2022-08-13 14:32:04 +0800

    up

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -3,10 +3,19 @@ import PageNotFound from "./errors/PageNotFound.vue"
 import Home from "./home/Home.vue"
 import Register from "./register/Register.vue"
 import Login from "./login/Login.vue"
-
+import LoginEmailConfirmationSuccessPage from "./login/LoginEmailConfirmationSuccessPage.vue"
+import RegisterEmailConfirmationSuccessPage from "./register/RegisterEmailConfirmationSuccessPage.vue"
 export const routes: Array<RouteRecordRaw> = [
   { path: "/", component: Home },
   { path: "/register", component: Register },
   { path: "/login", component: Login },
+  {
+    path: "/notifications/login-email-confirmation-success",
+    component: LoginEmailConfirmationSuccessPage,
+  },
+  {
+    path: "/notifications/register-email-confirmation-success",
+    component: RegisterEmailConfirmationSuccessPage,
+  },
   { path: "/:slugs(.*)*", component: PageNotFound },
 ]
