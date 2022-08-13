d32b244369d8aa99c5137dcdd5732d06fd7d9d84
Xie Yuheng <xyheme@gmail.com>
2022-08-13 19:36:38 +0800

    up

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { reactive } from "vue"
+import PageLayout from "../../layouts/page-layout/PageLayout.vue"
 import LoginStart from "./LoginStart.vue"
 import { LoginState as State } from "./LoginState"
 import LoginVerifying from "./LoginVerifying.vue"
-import PageLayout from "../../layouts/page-layout/PageLayout.vue"
 
 const state = reactive(new State())
 
