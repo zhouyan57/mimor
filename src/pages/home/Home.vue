8a822fc56060414e8d59e61fd6c7f0eeaca7ef1b
Xie Yuheng <xyheme@gmail.com>
2022-08-16 22:49:09 +0800

    up

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -25,15 +25,15 @@ const state = reactive(new State())
         <template #zh>
           <div class="flex flex-col py-2 space-y-4 max-w-xl">
             <h1 class="font-logo text-xl font-bold">主动回顾</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               在学习时，主动地将想要记住的点拆解为问答卡片，可以在之后作为测试来回顾。
             </p>
             <h1 class="font-logo text-xl font-bold">间隔重复</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               在回顾时，卡片将被分散于时间，并且间隔逐渐稀疏，以此来形成长期记忆。
             </p>
             <h1 class="font-logo text-xl font-bold">自我推荐</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               现代网络程序都有推荐系统，争相获取人们的注意力。我们也可以通过自己推荐给将来的自己，来管理自己的注意力。
             </p>
           </div>
@@ -41,17 +41,17 @@ const state = reactive(new State())
         <template #en>
           <div class="flex flex-col py-2 space-y-4 max-w-xl">
             <h1 class="font-logo text-xl font-bold">Active recall</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               When learning, when want to remember something, actively making
               them cards, to test ourself in recall.
             </p>
             <h1 class="font-logo text-xl font-bold">Spaced repetition</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               During recall, cards will be spaced in time, more and more sparse,
               to gain long-term memory.
             </p>
             <h1 class="font-logo text-xl font-bold">Recommending to ourself</h1>
-            <p class="text-lg">
+            <p class="text-xl">
               Modern web apps all have "recommender systems" to grab ones
               attention. We can also manage back our attention by recommending
               to ourself.
