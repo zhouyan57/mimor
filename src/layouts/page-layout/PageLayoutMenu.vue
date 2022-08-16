7299ba1bfd057784af59a4cc99f98de7f3cbe443
Xie Yuheng <xyheme@gmail.com>
2022-08-16 17:59:09 +0800

    up

--- src/layouts/page-layout/PageLayoutMenu.vue
+++ src/layouts/page-layout/PageLayoutMenu.vue
@@ -13,7 +13,7 @@ defineProps<{ state: State }>()
 <template>
   <Menu as="div" class="relative flex text-2xl">
     <MenuButton>
-      <MenuIcon class="h-5 w-5" />
+      <MenuIcon class="h-7 w-7" />
     </MenuButton>
 
     <Transition
