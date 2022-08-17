29c5df45a54d2a72c6139958c3506a551dcdbdb8
Xie Yuheng <xyheme@gmail.com>
2022-08-17 13:51:23 +0800

    up

--- src/layouts/page-layout/PageLayoutHeader.vue
+++ src/layouts/page-layout/PageLayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import PageLayoutMenu from './PageLayoutMenu.vue'
+import PageLayoutLogo from './PageLayoutLogo.vue'
 import { PageLayoutState as State } from './PageLayoutState'
 
 defineProps<{ state: State }>()
@@ -10,7 +11,11 @@ defineProps<{ state: State }>()
     <PageLayoutMenu class="block md:hidden pr-1" :state="state" />
 
     <div class="text-2xl font-bold font-logo">
-      <slot name="title" />
+      <slot name="title">
+        <div class="md:hidden block">
+          <PageLayoutLogo :state="state" />
+        </div>
+      </slot>
     </div>
   </div>
 </template>
