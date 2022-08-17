5f0cbfe974aa550938d2bf51ea46722ae61290f3
Xie Yuheng <xyheme@gmail.com>
2022-08-18 00:35:06 +0800

    up

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex h-screen w-screen flex-col items-center justify-center text-3xl text-white"
+    class="flex h-screen w-screen flex-col items-center justify-center text-xl"
   >
     <slot />
   </div>
