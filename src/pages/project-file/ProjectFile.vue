23cf7d6b915e8d6fbada58068138e2bc5098962a
Xie Yuheng <xyheme@gmail.com>
2022-08-17 19:05:30 +0800

    ProjectFileState -- take `path` and `project.name`

--- src/pages/project-file/ProjectFile.vue
+++ src/pages/project-file/ProjectFile.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
 import { reactive } from 'vue'
+import { useRoute } from 'vue-router'
 import Lang from '../../components/Lang.vue'
 import PageLayout from '../../layouts/page-layout/PageLayout.vue'
 import { ProjectFileState as State } from './ProjectFileState'
 
-const state = reactive(new State())
+const route = useRoute()
+
+const state = reactive(
+  new State({
+    path: formatPath(route.params.pathParts),
+    project: {
+      name: route.params.name,
+    },
+  })
+)
 
 function formatPath(pathParts: string | Array<string>): string {
   return typeof pathParts === 'string' ? pathParts : pathParts.join('/')
@@ -16,19 +26,18 @@ function formatPath(pathParts: string | Array<string>): string {
   <PageLayout mode="auth" :options="{ onInitialized: () => state.load() }">
     <Head>
       <title v-if="$app.lang.zh">
-        {{ formatPath($route.params.pathParts) }} | 谜墨
+        {{ state.path }} | 谜墨
       </title>
-      <title v-else>{{ formatPath($route.params.pathParts) }} | Mimor</title>
+      <title v-else>{{ state.path }} | Mimor</title>
     </Head>
 
     <template #title>
-      <div class="font-sans">{{ formatPath($route.params.pathParts) }}</div>
+      <div class="font-sans">{{ state.path }}</div>
     </template>
 
     <div>
-      <div>ProjectFile</div>
-      <div>{{ $route.params.name }}</div>
-      <div>{{ formatPath($route.params.pathParts) }}</div>
+      <div>{{ state.project.name }}</div>
+      <div>{{ state.path }}</div>
     </div>
   </PageLayout>
 </template>
--- src/pages/project-file/ProjectFileState.ts
+++ src/pages/project-file/ProjectFileState.ts
@@ -1,7 +1,26 @@
+import { FileJson } from '../../jsons/FileJson'
+
 export class ProjectFileState {
-  //
+  file?: FileJson
+
+  constructor(
+    public options: {
+      path: string
+      project: {
+        name: string
+      }
+    }
+  ) {}
+
+  get project() {
+    return this.options.project
+  }
+
+  get path() {
+    return this.options.path
+  }
 
   async load() {
-    //
+    // this.file = app.api.projectFile(this.project.name, this.path)
   }
 }
