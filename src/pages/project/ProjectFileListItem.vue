c7707c0290f9fd6a85840ba35e86e47fe43ceab8
Xie Yuheng <xyheme@gmail.com>
2022-08-19 02:54:22 +0800

    up

--- src/pages/project/ProjectFileListItem.vue
+++ src/pages/project/ProjectFileListItem.vue
@@ -11,13 +11,13 @@ defineProps<{ state: State; file: FileJson }>()
   <div class="flex w-full justify-between py-0.5">
     <Link
       :href="`/projects/${state.name}/files/${file.path}`"
-      class="max-w-sm overflow-x-auto overflow-y-hidden whitespace-pre font-bold hover:underline"
+      class="overflow-x-auto overflow-y-hidden whitespace-pre font-bold hover:underline"
     >
       {{ file.path }}
     </Link>
 
     <div class="flex grow">
-      <div class="flex grow flex-col justify-center pl-2 pr-0.5 pt-0.5">
+      <div class="flex grow flex-col justify-center pl-1.5 pr-0.5 pt-0.5">
         <div class="w-full border-b border-dashed border-stone-900"></div>
         <div class="w-full border-t border-dashed border-stone-900"></div>
       </div>
