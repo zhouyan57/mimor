14927ab3d7096cd39c050f5f2c8a79fff26d2a70
Xie Yuheng <xyheme@gmail.com>
2022-08-23 03:24:18 +0800

    up

--- src/utils/poll.ts
+++ src/utils/poll.ts
@@ -4,21 +4,27 @@ export function poll<A>(options: {
   target: () => Promise<A>
   check: (data: A) => Promise<boolean>
   then: (data: A) => Promise<void>
+  catch?: (error: Error) => Promise<void>
   interval: number
 }): { stop: () => void } {
-  const { target, check, then, interval } = options
-
   let polling: TimerId | null = null
 
   const stop = () => polling && clearInterval(polling)
 
   polling = setInterval(async () => {
-    const data = await target()
-    if (await check(data)) {
-      await then(data)
-      stop()
+    try {
+      const data = await options.target()
+      if (await options.check(data)) {
+        await options.then(data)
+        stop()
+      }
+    } catch (error) {
+      if (!(error instanceof Error)) throw error
+      if (options.catch) {
+        options.catch(error)
+      }
     }
-  }, interval)
+  }, options.interval)
 
   return { stop }
 }
