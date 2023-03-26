a2632e0d4960732006b3f1d64c3b23fcb8bf7ec0
Xie Yuheng <xyheme@gmail.com>
2023-03-27 04:08:48 +0800

    up

--- src/components/mimor/stateMaybeApplyEffect.ts
+++ src/components/mimor/stateMaybeApplyEffect.ts
@@ -5,25 +5,15 @@ export function stateMaybeApplyEffect(state: State): void {
   const who = 'stateMaybeApplyEffect'
 
   const program = state.program
-
   if (program === undefined) {
     return
   }
 
   const element = program.currentElement
-
   const effect = program.router.findEffect(element)
 
   if (effect) {
-    console.log({
-      who,
-      pointer: program.pointer,
-      tag: element.tag,
-      element,
-    })
-
     effect({ state, program, element })
-
     programNext(program)
   }
 }
