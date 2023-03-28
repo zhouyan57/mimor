f4500f7ac5591b77bbdd8addb294b600fe916c0f
Xie Yuheng <xyheme@gmail.com>
2023-03-29 04:08:20 +0800

    setup public/contents/examples/

--- src/pages/home/loadState.ts
+++ src/pages/home/loadState.ts
@@ -10,18 +10,18 @@ export async function loadState(options: StateOptions): Promise<State> {
   const origin = window.location.origin
 
   const list = [
-    `${origin}/contents/sisuo.mimor`,
-    `${origin}/contents/causal-reasoning.mimor`,
-    `${origin}/contents/do-not-forget.mimor`,
-    `${origin}/contents/99bottles/01-rediscovering-simplicity.mimor`,
-    `${origin}/contents/99bottles/02-test-driving-shameless-green.mimor`,
-    `${origin}/contents/99bottles/03-unearthing-concepts.mimor`,
-    `${origin}/contents/99bottles/04-practicing-horizontal-refactoring.mimor`,
-    `${origin}/contents/99bottles/05-separating-responsibilities.mimor`,
-    `${origin}/contents/99bottles/06-achieving-openness.mimor`,
-    `${origin}/contents/99bottles/07-manufacturing-intelligence.mimor`,
-    `${origin}/contents/99bottles/08-developing-a-programming-aesthetic.mimor`,
-    `${origin}/contents/99bottles/09-reaping-the-benefits-of-design.mimor`,
+    `${origin}/contents/examples/test.mimor`,
+    `${origin}/contents/examples/causal-reasoning.mimor`,
+    `${origin}/contents/examples/do-not-forget.mimor`,
+    `${origin}/contents/examples/99bottles/01-rediscovering-simplicity.mimor`,
+    `${origin}/contents/examples/99bottles/02-test-driving-shameless-green.mimor`,
+    `${origin}/contents/examples/99bottles/03-unearthing-concepts.mimor`,
+    `${origin}/contents/examples/99bottles/04-practicing-horizontal-refactoring.mimor`,
+    `${origin}/contents/examples/99bottles/05-separating-responsibilities.mimor`,
+    `${origin}/contents/examples/99bottles/06-achieving-openness.mimor`,
+    `${origin}/contents/examples/99bottles/07-manufacturing-intelligence.mimor`,
+    `${origin}/contents/examples/99bottles/08-developing-a-programming-aesthetic.mimor`,
+    `${origin}/contents/examples/99bottles/09-reaping-the-benefits-of-design.mimor`,
   ]
 
   return {
