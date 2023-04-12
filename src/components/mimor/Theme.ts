39de5a119335b4c531a7d614809667d1d03f9ff9
Xie Yuheng <xyheme@gmail.com>
2023-04-12 22:09:45 +0800

    up

--- src/components/mimor/Theme.ts
+++ src/components/mimor/Theme.ts
@@ -12,10 +12,6 @@ export class Theme {
       name = 'sky'
     }
 
-    if (name === 'yellow') {
-      name = 'amber'
-    }
-
     return (tailwindColors as any)[name]
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /(text|border|bg)-(red|orange|yellow|amber|green|blue|indigo|purple|stone)-(50|100|200|300|400|500|600|700|800|900)/,
+        /(text|border|bg)-(red|orange|yellow|green|blue|indigo|purple|stone)-(50|100|200|300|400|500|600|700|800|900)/,
       variants: ['hover', 'focus'],
     },
   ],
