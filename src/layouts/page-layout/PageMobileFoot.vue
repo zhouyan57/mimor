1ba6d897b4c6654fc8fdd8805ca42070cc272a8a
Xie Yuheng <xyheme@gmail.com>
2023-04-24 19:11:45 +0800

    up

--- src/layouts/page-layout/PageMobileFoot.vue
+++ src/layouts/page-layout/PageMobileFoot.vue
@@ -7,7 +7,7 @@ const router = useRouter()
 </script>
 
 <template>
-  <div class="flex w-full justify-between bg-white px-4 py-1">
+  <div class="flex w-full justify-between bg-white px-4 py-1 opacity-90">
     <div></div>
     <PageMobileMenu class="rounded border border-stone-300 p-2" />
     <div></div>
