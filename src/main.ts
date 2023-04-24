fcd395fa0b5fe9902e919bd91ebcd2edab517315
Xie Yuheng <xyheme@gmail.com>
2023-04-24 17:45:01 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createHead } from '@vueuse/head'
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as Directives from './directives'
 import router from './router'
 import './styles/fonts.css'
 import './styles/index.css'
@@ -9,5 +10,11 @@ const app = createApp(App)
 
 app.use(router)
 app.use(createHead())
+
+app.directive('focus', Directives.focus)
+app.directive('blur', Directives.blur)
+app.directive('created', Directives.created)
+app.directive('mounted', Directives.mounted)
+app.directive('on-click-outside', Directives.onClickOutside)
 
 app.mount('#app')
