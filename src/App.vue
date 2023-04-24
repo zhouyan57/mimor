e91788ba2737a1914657a40eff1da3480f1dcdc2
Xie Yuheng <xyheme@gmail.com>
2023-04-24 19:46:26 +0800

    `App` -- try to `loginByToken`

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
+import { onMounted } from 'vue'
 import { useGlobalLang } from './components/lang/useGlobalLang'
+import { loginByToken } from './reactives/loginByToken'
+import { useGlobalAuth } from './reactives/useGlobalAuth'
+import { useGlobalToken } from './reactives/useGlobalToken'
 
 const lang = useGlobalLang()
+const auth = useGlobalAuth()
+const token = useGlobalToken()
+
+const report = {
+  errorMessage: '',
+}
+
+onMounted(async () => {
+  if (!token.name) {
+    return
+  }
+
+  if (auth.username) {
+    auth.isLoadingUser = true
+
+    try {
+      await loginByToken({ username: auth.username }, report)
+    } catch (_error) {}
+
+    auth.isLoadingUser = false
+  }
+})
 </script>
 
 <template>
