66681ffe10a0689ab9f29d3e8ed1cc168d186a6a
Xie Yuheng <xyheme@gmail.com>
2023-04-26 15:37:52 +0800

    up

--- src/pages/author/AuthorEditor.vue
+++ src/pages/author/AuthorEditor.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useForm } from '../../components/form'
 import Lang from '../../components/lang/Lang.vue'
 import { useGlobalLang } from '../../components/lang/useGlobalLang'
 
 defineProps<{ username: string }>()
 
+const editing = ref(false)
 const lang = useGlobalLang()
 
 const form = useForm({
@@ -23,20 +24,25 @@ function numberOfLines() {
 </script>
 
 <template>
-  <form class="flex flex-col border border-black p-2">
+  <form
+    class="flex flex-col border border-black p-2"
+    :class="{ 'border-orange-400 ring-2 ring-orange-300': editing }"
+  >
     <textarea
-      :placeholder="lang.isZh() ? '创作卡片⋯⋯' : 'Create cards...'"
-      spellcheck="false"
       class="my-1.5 h-full w-full resize-none px-1.5 font-mono focus:outline-none"
       name="text"
-      :rows="numberOfLines()"
+      spellcheck="false"
+      @focus="editing = true"
+      @blur="editing = false"
       v-model="form.values.text"
+      :rows="numberOfLines()"
+      :placeholder="lang.isZh() ? '创作卡片⋯⋯' : 'Create cards...'"
     ></textarea>
 
     <div class="flex justify-end">
       <button
         :disabled="form.values.text.length === 0"
-        class="border border-orange-200 bg-orange-400 px-2 py-1 font-sans font-bold text-orange-50 disabled:border-stone-400 disabled:bg-white disabled:text-stone-400"
+        class="border border-orange-300 bg-orange-400 px-2 py-1 font-sans font-bold text-orange-50 disabled:border-stone-400 disabled:bg-white disabled:text-stone-400"
       >
         <Lang>
           <template #zh>保存</template>
