59dcaa66d2ab5d5b9baa33c618a8aac4b4c83299
Xie Yuheng <xyheme@gmail.com>
2023-04-30 17:25:12 +0800

    up

--- src/pages/author/AuthorEditor.vue
+++ src/pages/author/AuthorEditor.vue
@@ -80,12 +80,17 @@ const report = reactive({
       <div class="flex space-x-2">
         <button
           v-if="state.editor.isPublic"
+          class="px-1 focus:outline-none focus:ring-2 focus:ring-orange-300"
           @click.prevent="state.editor.isPublic = false"
         >
           <LockOpenIcon class="h-5 w-5" />
         </button>
 
-        <button v-else @click.prevent="state.editor.isPublic = true">
+        <button
+          v-else
+          class="px-1 focus:outline-none focus:ring-2 focus:ring-orange-300"
+          @click.prevent="state.editor.isPublic = true"
+        >
           <LockClosedIcon class="h-5 w-5" />
         </button>
       </div>
@@ -93,7 +98,7 @@ const report = reactive({
       <div class="flex px-1">
         <button
           :disabled="state.editor.text.length === 0"
-          class="border border-orange-300 bg-orange-400 px-3 py-1 text-orange-50 disabled:border-stone-400 disabled:bg-white disabled:text-stone-400"
+          class="border border-orange-300 bg-orange-400 px-3 py-1 text-orange-50 focus:outline-none focus:ring-2 focus:ring-orange-300 disabled:border-stone-400 disabled:bg-white disabled:text-stone-400"
         >
           <PaperAirplaneIcon class="h-5 w-5" />
         </button>
