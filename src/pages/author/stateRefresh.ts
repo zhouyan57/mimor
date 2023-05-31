6d011b40b2ae9ee3ea330b3a51a46205e787e29c
Xie Yuheng <xyheme@gmail.com>
2023-05-31 09:28:44 +0800

    utils/vue/reactiveToRaw

--- src/pages/author/stateRefresh.ts
+++ src/pages/author/stateRefresh.ts
@@ -1,4 +1,5 @@
 import * as Kv from 'idb-keyval'
+import { reactiveToRaw } from '../../utils/vue/reactiveToRaw'
 import { State } from './State'
 import { loadState } from './loadState'
 
@@ -7,6 +8,6 @@ export async function stateRefresh(state: State): Promise<void> {
   const store = Kv.createStore('mimor.app/authors', 'cache')
   const newState = await loadState(state)
   Object.assign(state, newState)
-  await Kv.set(state.username, newState, store)
+  await Kv.set(state.username, reactiveToRaw(state), store)
   state.isRefreshing = false
 }
