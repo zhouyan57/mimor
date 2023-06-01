13f4269932da90456f181bdb0567085d6f827a33
Xie Yuheng <xyheme@gmail.com>
2023-06-01 17:18:13 +0800

    [author] `stateRefresh` -- only update entries that have different `text`
    - quit using `Object.assign`

--- src/pages/author/stateReactive.ts
+++ src/pages/author/stateReactive.ts
@@ -1,14 +1,13 @@
 import { reactive } from 'vue'
 import { asyncRun } from '../../utils/asyncRun'
 import { State } from './State'
-import { stateRefresh } from './stateRefresh'
 
 export function stateReactive(state: State): State {
   state = reactive(state) as State
 
   if (state.isLoadedFromCache) {
     asyncRun(async () => {
-      await stateRefresh(state)
+      //      await stateRefresh(state)
     })
   }
 
--- src/pages/author/stateRefresh.ts
+++ src/pages/author/stateRefresh.ts
@@ -6,12 +6,21 @@ export async function stateRefresh(state: State): Promise<void> {
   state.isRefreshing = true
 
   const newState = await loadState(state)
-  Object.assign(state, newState)
 
-  await stateCacheSave({
-    ...newState,
-    latestDirectoryHandle: state.latestDirectoryHandle,
-  })
+  state.user = newState.user
+  for (const newEntry of newState.entries) {
+    const found = state.entries.find((entry) => entry.path === newEntry.path)
+    if (found) {
+      if (found.text !== newEntry.text) {
+        found.text = newEntry.text
+        found.updatedAt = newEntry.updatedAt
+      }
+    } else {
+      state.entries.push(newEntry)
+    }
+  }
+
+  await stateCacheSave(state)
 
   state.isRefreshing = false
 }
