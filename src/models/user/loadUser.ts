4a84b8449f8d4effe36d834035b21c49316c5cf5
Xie Yuheng <xyheme@gmail.com>
2023-06-04 05:06:59 +0800

    up

--- src/models/user/loadUser.ts
+++ src/models/user/loadUser.ts
@@ -1,3 +1,4 @@
+import { Base64 } from 'js-base64'
 import { User } from '../../models/user/User'
 import { useGlobalBackend } from '../../reactives/useGlobalBackend'
 import { loadUserAvatar } from './loadUserAvatar'
@@ -11,6 +12,7 @@ export async function loadUser(username: string): Promise<User> {
 
   const user = await response.json()
   const avatar = await loadUserAvatar(user)
+  const avatarBase64 = avatar && Base64.fromUint8Array(avatar)
 
-  return { ...user, avatar }
+  return { ...user, avatar, avatarBase64 }
 }
