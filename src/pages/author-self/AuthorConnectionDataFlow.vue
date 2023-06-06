1c29eb3935368b3f57fec856d622209c921e22cc
Xie Yuheng <xyheme@gmail.com>
2023-06-06 17:47:41 +0800

    up

--- src/pages/author-self/AuthorConnectionDataFlow.vue
+++ src/pages/author-self/AuthorConnectionDataFlow.vue
@@ -27,28 +27,28 @@ defineProps<{
     <Lang class="px-5">
       <template #zh>
         <pre>
-      「云端」
-       /   ↖
-<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowPathIcon class="w-5 h-5" /> 刷新</button>   <button class="inline-flex items-center border border-black py-1 px-1.5"><PaperAirplaneIcon class="w-5 h-5" /> 保存</button>
-       ↘   /
-       「App」
-       /   ↖  <button class="inline-flex items-center border border-black my-1 py-1 px-1.5"  @click="stateDisconnect(state)"><ScissorsIcon class="w-5 h-5 rotate-180" /> 断开连接</button>
-<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowDownTrayIcon class="w-5 h-5" /> 下载</button>   <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowUpTrayIcon class="w-5 h-5" /> 上传</button>
-       ↘   /
-    「本地文件夹」
+       「云端」
+        /   ↖
+<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowPathIcon class="w-5 h-5" /> 刷新</button>    <button class="inline-flex items-center border border-black py-1 px-1.5"><PaperAirplaneIcon class="w-5 h-5" /> 保存</button>
+        ↘   /
+        「App」
+        /   ↖  <button class="inline-flex items-center border border-black my-1 py-1 px-1.5"  @click="stateDisconnect(state)"><ScissorsIcon class="w-5 h-5 rotate-180" /> 断开连接</button>
+<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowDownTrayIcon class="w-5 h-5" /> 下载</button>    <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowUpTrayIcon class="w-5 h-5" /> 上传</button>
+        ↘   /
+     「本地文件夹」
 </pre>
       </template>
       <template #en>
         <pre>
-         [Cloud]
-          /   ↖
- <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowPathIcon class="w-5 h-5" /> Refresh</button> <button class="inline-flex items-center border border-black py-1 px-1.5"><PaperAirplaneIcon class="w-5 h-5" /> Save</button>
-          ↘   /
-          [App]
-          /   ↖  <button class="inline-flex items-center border border-black my-1 py-1 px-1.5" @click="stateDisconnect(state)"><ScissorsIcon class="w-5 h-5 rotate-180" /> Disconnect</button>
-<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowDownTrayIcon class="w-5 h-5" /> Download</button> <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowUpTrayIcon class="w-5 h-5" /> Upload</button>
-          ↘   /
-       [Directory]
+           [Cloud]
+            /   ↖
+ <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowPathIcon class="w-5 h-5" /> Refresh</button>    <button class="inline-flex items-center border border-black py-1 px-1.5"><PaperAirplaneIcon class="w-5 h-5" /> Save</button>
+            ↘   /
+            [App]
+            /   ↖  <button class="inline-flex items-center border border-black my-1 py-1 px-1.5" @click="stateDisconnect(state)"><ScissorsIcon class="w-5 h-5 rotate-180" /> Disconnect</button>
+<button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowDownTrayIcon class="w-5 h-5" /> Download</button>    <button class="inline-flex items-center border border-black py-1 px-1.5"><ArrowUpTrayIcon class="w-5 h-5" /> Upload</button>
+            ↘   /
+         [Directory]
 </pre>
       </template>
     </Lang>
