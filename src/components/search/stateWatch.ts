db84ab96742c838a508688ed5e4764fd96640a9c
Xie Yuheng <xyheme@gmail.com>
2023-06-09 18:13:54 +0800

    Revert "up"
    This reverts commit 5edcaa655ddaa0e3a82f7990d58812fc52e474f6.

--- src/components/search/stateWatch.ts
+++ src/components/search/stateWatch.ts
@@ -1,4 +1,4 @@
-import { debounce } from 'lodash'
+// import { debounce } from 'lodash'
 import { watch } from 'vue'
 import { State } from './State'
 import { stateSearch } from './stateSearch'
@@ -6,12 +6,14 @@ import { stateSearch } from './stateSearch'
 export function stateWatch(state: State): void {
   watch(
     () => state.text,
-    debounce(() => stateSearch(state), 300),
+    // debounce(() => stateSearch(state), 300),
+    () => stateSearch(state),
     { immediate: true },
   )
 
   watch(
     () => state.keywords,
+    // debounce(() => stateSearch(state), 300),
     () => stateSearch(state),
     { immediate: true, deep: true },
   )
