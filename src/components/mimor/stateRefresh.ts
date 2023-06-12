9c2c65d74b56369a4592ef10b8a2c40add82f505
Xie Yuheng <xyheme@gmail.com>
2023-06-12 11:01:57 +0800

    [mimor] keep program pointer on refresh

--- src/components/mimor/stateRefresh.ts
+++ src/components/mimor/stateRefresh.ts
@@ -9,7 +9,15 @@ export async function stateRefresh(state: State): Promise<void> {
   const text = await loadContent(state.src)
   // We need `stateReactive` to make nested objects reactive.
   const newState = stateReactive(createState({ ...state, text }))
+
+  const oldPointer = state.program?.pointer
+
   Object.assign(state, newState)
+
+  if (state.program && oldPointer) {
+    state.program.pointer = oldPointer
+  }
+
   const cached = { text }
   await Kv.set(state.src, cached, store)
 }
