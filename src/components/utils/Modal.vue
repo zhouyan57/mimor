d8dc3489d3089c8dae39b8217dc547326e41306c
Xie Yuheng <xyheme@gmail.com>
2023-06-17 07:51:25 +0800

    extract `components/utils/TrapFocus`

--- src/components/utils/Modal.vue
+++ src/components/utils/Modal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { createFocusTrap, FocusTrap } from 'focus-trap'
-import { nextTick, onMounted, reactive, ref, watch } from 'vue'
+import { reactive } from 'vue'
+import TrapFocus from './TrapFocus.vue'
 
 /*
 
@@ -18,64 +18,36 @@ import { nextTick, onMounted, reactive, ref, watch } from 'vue'
 const state = reactive({
   open: false,
 })
-
-const buttonElement = ref<HTMLButtonElement | undefined>()
-const panelElement = ref<HTMLDivElement | undefined>()
-const modalElement = ref<HTMLDivElement | undefined>()
-
-const focusTrap = ref<FocusTrap | undefined>()
-
-onMounted(() => {
-  if (modalElement.value) {
-    focusTrap.value = createFocusTrap(modalElement.value)
-  }
-})
-
-watch(
-  () => state.open,
-  async (value) => {
-    if (!focusTrap.value) return
-
-    if (value) {
-      await nextTick()
-      focusTrap.value.activate()
-    } else {
-      await nextTick()
-      focusTrap.value.deactivate()
-    }
-  },
-  { immediate: true },
-)
 </script>
 
 <template>
   <div @keydown.escape.prevent.stop="state.open = false">
-    <button ref="buttonElement" type="button" @click="state.open = true">
+    <button type="button" @click="state.open = true">
       <slot name="button" :modal="state"></slot>
     </button>
 
     <div
       v-show="state.open"
-      ref="modalElement"
       class="h-screen-dynamic fixed inset-0 z-50"
       role="dialog"
       aria-modal="true"
     >
       <div class="fixed inset-0 bg-black bg-opacity-50"></div>
 
-      <div
-        ref="panelElement"
-        tabindex="0"
-        class="h-screen-dynamic relative flex flex-col items-center justify-center overflow-y-auto"
-        @click="state.open = false"
-      >
+      <TrapFocus :open="state.open">
         <div
-          @click.stop
-          class="flex h-fit flex-col items-center justify-center overflow-y-auto"
+          tabindex="0"
+          class="h-screen-dynamic relative flex flex-col items-center justify-center overflow-y-auto"
+          @click="state.open = false"
         >
-          <slot name="panel" :modal="state"></slot>
+          <div
+            @click.stop
+            class="flex h-fit flex-col items-center justify-center overflow-y-auto"
+          >
+            <slot name="panel" :modal="state"></slot>
+          </div>
         </div>
-      </div>
+      </TrapFocus>
     </div>
   </div>
 </template>
