e4b106057f007bf1a4f92ee0bb38101f618d0ee0
Xie Yuheng <xyheme@gmail.com>
2023-06-23 02:05:21 +0800

    [recall] `RecallLoading` use `PageLayout`

--- src/pages/recall/RecallLoading.vue
+++ src/pages/recall/RecallLoading.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import PageLayout from '../../layouts/page-layout/PageLayout.vue'
 import Lang from '../../components/lang/Lang.vue'
 import { StateOptions } from './loadState'
 
@@ -6,14 +7,16 @@ defineProps<{ options: StateOptions }>()
 </script>
 
 <template>
-  <div class="font-sans bg-white px-4 py-6 text-xl text-stone-500">
-    <Lang>
-      <template #zh>「回顾」加载中⋯⋯</template>
-      <template #en>Loading "Recall" ...</template>
-    </Lang>
+  <PageLayout>
+    <div class="font-sans bg-white p-3 text-xl text-stone-500">
+      <Lang>
+        <template #zh>「回顾」加载中⋯⋯</template>
+        <template #en>Loading "Recall" ...</template>
+      </Lang>
 
-    <div class="font-mono overflow-x-auto whitespace-pre py-1 text-sm">
-      {{ options }}
+      <div class="font-mono overflow-x-auto whitespace-pre py-1 text-sm">
+        {{ options }}
+      </div>
     </div>
-  </div>
+  </PageLayout>
 </template>
