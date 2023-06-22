cc492c69ba4ccfd820e3d45e50d62a539185330a
Xie Yuheng <xyheme@gmail.com>
2023-06-22 21:53:37 +0800

    up

--- src/pages/author-self/entryCompare.ts
+++ src/pages/author-self/entryCompare.ts
@@ -1,6 +1,14 @@
 import { Entry } from './Entry'
 
 export function entryCompare(x: Entry, y: Entry): number {
+  if (y.isNotInTheCloud && !x.isNotInTheCloud) {
+    return 1
+  }
+
+  if (!y.isNotInTheCloud && x.isNotInTheCloud) {
+    return -1
+  }
+
   if (y.isModifiedByUploading && !x.isModifiedByUploading) {
     return 1
   }
